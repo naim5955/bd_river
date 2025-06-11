1c496ea776bef34cde38177e75395a391f01d30a
naim5955 <86704734+naim5955@users.noreply.github.com>
2025-06-11 15:40:42 +0600

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from titiler.application.main import TitilerApp
-
-app = FastAPI()
-
-# Mount the TiTiler app at root or any path you want:
-app.mount("/", TitilerApp())
